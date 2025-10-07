2c87bac7eff2ec2d1275dd8822193b8a521a31f9
Hugo Croisic <hugocroisic@gmail.com>
2025-10-07 14:49:40 +0200

    chore: fresh start (clean repo)

---
+++ include/hello.hpp
@@ -0,0 +1,3 @@
+﻿#pragma once
+#include <string>
+std::string greet(const std::string& name);
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include "hello.hpp"
+
+std::string greet(const std::string& name) { return "Hello, " + name + "!"; }
+
+int main() {
+    std::cout << greet("World") << std::endl;
+    return 0;
+}
